20fde10f28ea69f0fc61df0a486c5bc7c183784f
cwen-booth <cwen@chicagobooth.edu>
2023-01-24 08:24:54 +0000

    HW 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,48 @@
 class ApplicationController < ActionController::Base
+  def homepage
+    render({ :template => "/homepage.html.erb" })
+  end
 
+  def square
+    render({ :template => "/computations/square.html.erb" })
+  end
+
+  def square_result
+    @number = params["square_input"].to_i
+    @number_squared = params["square_input"].to_f ** 2
+    render({ :template => "/results/square.html.erb" })
+  end
+
+  def square_root
+    render({ :template => "/computations/square_root.html.erb" })
+  end
+
+  def square_root_result
+    @number = params["square_root_input"].to_i
+    @number_rooted = params["square_root_input"].to_f ** 0.5
+    render({ :template => "/results/square_root.html.erb" })
+  end
+
+  def payment
+    render({ :template => "/computations/payment.html.erb" })
+  end
+
+  def payment_result
+    @apr = params["apr_input"].to_f / 100
+    @nyears = params["nyears_input"].to_i
+    @principal = params["principal_input"].to_f
+    @payment = ((@apr / 12) * (@principal)) / (1 - (1 + (@apr / 12)) ** (-12 * @nyears))
+    render({ :template => "/results/payment.html.erb" })
+  end
+
+  def random
+    render({ :template => "/computations/random.html.erb" })
+  end
+
+  def random_result
+    @min = params["min_input"].to_f
+    @max = params["max_input"].to_f
+    @random_number = rand(@min..@max)
+    render({ :template => "/results/random.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "application", :action => "square" })
 
+  get("/square/new", { :controller => "application", :action => "square" })
+  get("/square/results", { :controller => "application", :action => "square_result" })
+
+  get("/square_root/new", { :controller => "application", :action => "square_root" })
+  get("/square_root/results", { :controller => "application", :action => "square_root_result" })
+
+  get("/payment/new", { :controller => "application", :action => "payment" })
+  get("/payment/results", { :controller => "application", :action => "payment_result" })
+
+  get("/random/new", { :controller => "application", :action => "random" })
+  get("/random/results", { :controller => "application", :action => "random_result" })
 end
